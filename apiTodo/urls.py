57dbb6ee5fa09d22cef3682e46cfea0bbbff6c85
henryfrstr <henryfrstr@gmail.com>
2021-11-11 02:44:05 +0300

    add apiview

--- apiTodo/urls.py
+++ apiTodo/urls.py
@@ -1,14 +1,25 @@
 from django.urls import path
-from .views import home, todoList, todoListCreate, toDo_list, todoListUpdate, todoListDelete, toDo_detail
+from .views import (home,
+                    todoList,
+                    todoListCreate,
+                    toDo_list,
+                    todoListUpdate,
+                    todoListDelete,
+                    toDo_detail,
+                    TodoList,
+                    TodoDetail
+                    )
 
 
 urlpatterns = [
     path('', home),
-    path('todoList/', todoList), 
-    path('todoListCreate/', todoListCreate),   
-    path('toDo_list/', toDo_list), 
-    path('todoListUpdate/<int:pk>/', todoListUpdate),  
-    path('todoListDelete/<int:pk>/', todoListDelete),    
-    path('toDo_detail/<int:pk>/', toDo_detail),  
-    
+    # path('todoList/', todoList),
+    # path('todoListCreate/', todoListCreate),
+    # path('toDo_list/', toDo_list),
+    # path('todoListUpdate/<int:pk>/', todoListUpdate),
+    # path('todoListDelete/<int:pk>/', todoListDelete),
+    # path('toDo_detail/<int:pk>/', toDo_detail),
+    path('todo/', TodoList.as_view()),
+    path('todo/<int:pk>', TodoDetail.as_view()),
+
 ]
